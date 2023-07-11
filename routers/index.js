f09d95bbee70038d29c92a43eaa2674beab4902e
sophia <zchen9523@gmail.com>
2023-07-11 16:22:35 -0500

    git something for week2 weekend lab

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+const path = require('path');
+const auth = require('http-auth');
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd'),
+});
 const {check, validationResult} = require("express-validator");
+const Registration = mongoose.model('Registration');
 router.get('/', function(req, res) {
     res.render('form',{title: 'Registration form'});
 });
+router.get('/registrations', basic.check((req,res) => {
+   Registration.find()
+       .then((registations)=>{
+           res.render('index', {title: "Listening registrations", registrations});
+       })
+       .catch(() =>{res.send('Sorry! Something went wrong.');});
+}));
 
 router.post('/',
     [
@@ -18,8 +32,16 @@ router.post('/',
         // console.log(req.body);
         const errors = validationResult(req);
         if (errors.isEmpty()) {
-            res.send('thank you for your registration!');
-        } else {
+            const registration = new Registration(req.body);
+            registration.save()
+                .then(() => {
+                    res.send('Thank you for your registration!');
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.send("Sorry! Something went wrong.");
+                });
+        }else {
             res.render('form', {
                 title:"Registration form",
                 errors: errors.array(),
