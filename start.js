d88e1a3e9656947312d6386a8f9db756d89a45b8
sophia <zchen9523@gmail.com>
2023-07-24 01:11:20 -0500

    something combine

---
+++ start.js
@@ -0,0 +1,20 @@
+
+require('dotenv').config();
+require('./models/Registration');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+});
+mongoose.connection
+    .on('open', ()=>{
+        console.log("Mongoose connection open");
+    })
+    .on('error', (err) => {
+        console.log(`connection error :${err.message}`);
+    });
+
+const app = require('./app');
+const server = app.listen(3000, function(){
+    console.log(`express is running on port ${server.address().port}`);
+});
